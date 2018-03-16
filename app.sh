943ef543b0526643763ab5620491df9938a84535
Nat Weerawan <nat@cmmc.io>
2018-03-16 17:50:56 +0700

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,10 +1,8 @@
 #!/bin/bash 
 
-echo "[$(date)]"
 REMOTE_PORT=`(cat /boot/cmmc_port.txt | tr -d '\n')`
 REMOTE_HOST=`(cat /boot/cmmc_host.txt | tr -d '\n')`
-echo "PORT=${REMOTE_PORT}"
-echo "HOST=${REMOTE_HOST}"
+
 /bin/pidof ssh > /dev/null
 if [[ $? -ne 0 ]]; then
   echo $(date)
@@ -13,4 +11,3 @@ if [[ $? -ne 0 ]]; then
 else
   echo "autossh is running"
 fi
-##@reboot /home/pi/autossh/autossh.sh > /home/pi/autossh.log 2>&1 
