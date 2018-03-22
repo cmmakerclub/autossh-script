7b3b530b461c337ce1aad307e56083ca7d910b35
Nat Weerawan <nat@cmmc.io>
2018-03-22 11:47:18 +0700

    add autossh config

--- cmmc.sh
+++ cmmc.sh
@@ -3,12 +3,26 @@ VERSION=0.0.1
 usage() {
         cat <<EOF
 cmmc v$VERSION
-Usage: $0 [--help]
+Usage: $0 [autossh|--help]
 EOF
         exit 1
 } 
 
+autossh_fn() {
+  usage() {
+        cat <<EOF
+    Usage: $0 $1 [config|help]
+EOF
+  }
+  case "$1" in
+    config) echo "enter config";;
+    *) usage "autossh";;
+  esac
+
+}
+
 case "$1" in
         --help|help) usage;;
+        autossh) autossh_fn $2;;
         *) usage;;
 esac
