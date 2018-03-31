891bb7852240cfd3020a8bdb41b83b5d31765950
Nat Weerawan <nat@cmmc.io>
2018-03-31 18:54:43 +0700

    Update cmmc.sh

--- cmmc.sh
+++ cmmc.sh
@@ -14,7 +14,7 @@ case "$1" in
         install) 
           echo "enter install"
           cd /home/pi
-          wget https://github.com/cmmakerclub/autossh-script/archive/v0.0.1.zip  -O- | tar xvz
+          wget https://github.com/cmmakerclub/autossh-script/archive/v0.0.1.tar.gz  -O- | tar xvz
           #sudo ln -fvs /home/$USER/autossh-script/cmmc.sh /usr/bin/cmmc
         ;;
         uninstall) 
