73b1e1f04c5c9740092f89d6e28b3689b47451c4
Nat <nat@cmmc.io>
2018-03-31 09:21:02 +0000

    update script

--- app.sh
+++ app.sh
@@ -2,6 +2,7 @@
 
 REMOTE_PORT=`(cat /boot/cmmc_ssh_port.txt | tr -d '\n')`
 REMOTE_HOST=`(cat /boot/cmmc_ssh_host.txt | tr -d '\n')`
+REMOTE_USER=device
 
 /bin/pidof ssh > /dev/null
 if [[ $? -ne 0 ]]; then
@@ -9,7 +10,8 @@ if [[ $? -ne 0 ]]; then
   echo "Restarting autossh service..."
   which autossh
   if [[ $? -eq 0 ]]; then
-    autossh -f -N -M 0 -o ServerAliveInterval=20 -R $REMOTE_PORT:127.0.0.1:22 $REMOTE_HOST -v 
+    autossh -f -N -M 0 -o ServerAliveInterval=20 -R $REMOTE_PORT:127.0.0.1:22 "$REMOTE_USER@$REMOTE_HOST" -v 
+    echo "$REMOTE_USER@$REMOTE_HOST" 
   else
     echo "autossh not found. getting installed autossh"
     sudo apt-get install autossh
