0b721d1cbca5dada3904fe1f303cfa1e4a11cdb5
Nat <nat@cmmc.io>
2018-03-31 08:38:19 +0000

    add ssh config and check

--- subtasks/_autossh.sh
+++ subtasks/_autossh.sh
@@ -7,20 +7,28 @@ cmmc autossh 0.0.1
 EOF
   }
   case "$1" in
-    config) 
-        unset host
+    config) unset host
         while [ -z ${host} ]; do
              read -e -r -p "Enter server host: " host
         done
         while :; do
-            read -rp 'Enter server port(2000-8000): ' number
-            [[ $number =~ ^[[:digit:]]+$ ]] || continue
-            (number=(10#$number))
-            (( (number<=8000) && number>=2000)) || continue
+            read -rp 'Enter server port(2000-8000): ' port
+            [[ $port =~ ^[[:digit:]]+$ ]] || continue
+            (port=(10#$port))
+            (((port<=8000) && (port>=2000))) || continue
             break
         done 
         echo "server host = ${host}"
-        echo "server port = ${number}" 
+        echo "server port = ${port}" 
+        sudo echo "${host}" > /boot/cmmc_ssh_host.txt
+        sudo echo "${port}" > /boot/cmmc_ssh_port.txt
+    ;;
+    check) 
+        echo "loading configurations"
+        host=`cat /boot/cmmc_ssh_host.txt`
+        port=`cat /boot/cmmc_ssh_port.txt`
+        echo "server host = ${host}"
+        echo "server port = ${port}" 
     ;;
     install) echo "enter install";;
     uninstall) echo "enter uninstall";;
