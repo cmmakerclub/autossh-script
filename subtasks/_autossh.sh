3465d8085c59e7f5fc93fa1589b86c21ba5cb734
Nat Weerawan <nat@cmmc.io>
2018-03-31 20:42:09 +0700

    Update _autossh.sh

--- subtasks/_autossh.sh
+++ subtasks/_autossh.sh
@@ -8,6 +8,10 @@ EOF
   }
   case "$1" in
     config) unset host
+        if [ "$(whoami)" != "root" ]; then
+           echo "Script must be run as user: root"
+           exit -1
+        fi
         while [ -z ${host} ]; do
              read -e -r -p "Enter server host: " host
         done
@@ -17,30 +21,30 @@ EOF
             (port=(10#$port))
             (((port>=22001) &&(port<=22999))) || continue
             break
-        done 
+        done
         echo "server host = ${host}"
-        echo "server port = ${port}" 
-        sudo echo "${host}" > /boot/cmmc_ssh_host.txt
-        sudo echo "${port}" > /boot/cmmc_ssh_port.txt
+        echo "server port = ${port}"
+        echo "${host}" > /boot/cmmc_ssh_host.txt
+        echo "${port}" > /boot/cmmc_ssh_port.txt
     ;;
     start)
        echo "starting cmmc-autossh"
       /home/$USER/autossh-script/cmmc-autossh.sh
     ;;
-    check) 
+    check)
         echo "loading configurations"
         host=`cat /boot/cmmc_ssh_host.txt`
         port=`cat /boot/cmmc_ssh_port.txt`
         echo "server host = ${host}"
-        echo "server port = ${port}" 
+        echo "server port = ${port}"
     ;;
-    install) 
+    install)
       echo "installing crontab.d"
-      sudo cp -Rvf /home/$USER/autossh-script/cron.d/cmmc_autossh /etc/cron.d/cmmc_autossh 
+      sudo cp -Rvf /home/$USER/autossh-script/cron.d/cmmc_autossh /etc/cron.d/cmmc_autossh
     ;;
-    uninstall) 
+    uninstall)
       echo "enter uninstall"
-      sudo rm -f /etc/cron.d/cmmc_autossh 
+      sudo rm -f /etc/cron.d/cmmc_autossh
     ;;
     *) usage "autossh";;
   esac
