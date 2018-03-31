3e7c81665ad5e53ea30f4f5262fa7e4be25b79c5
Nat <nat@cmmc.io>
2018-03-31 08:42:53 +0000

    add install autossh automatically

--- app.sh
+++ app.sh
@@ -1,13 +1,20 @@
 #!/bin/bash 
 
-REMOTE_PORT=`(cat /boot/cmmc_port.txt | tr -d '\n')`
-REMOTE_HOST=`(cat /boot/cmmc_host.txt | tr -d '\n')`
+REMOTE_PORT=`(cat /boot/cmmc_ssh_port.txt | tr -d '\n')`
+REMOTE_HOST=`(cat /boot/cmmc_ssh_host.txt | tr -d '\n')`
 
 /bin/pidof ssh > /dev/null
 if [[ $? -ne 0 ]]; then
   echo $(date)
   echo "Restarting autossh service..."
-  autossh -f -N -M 0 -o ServerAliveInterval=20 -R $REMOTE_PORT:127.0.0.1:22 $REMOTE_HOST -v
+  which autossh
+  if [[ $? -eq 0 ]]; then
+    autossh -f -N -M 0 -o ServerAliveInterval=20 -R $REMOTE_PORT:127.0.0.1:22 $REMOTE_HOST -v 
+  else
+    echo "autossh not found. getting installed autossh"
+    sudo apt-get install autossh
+  fi
+
 else
   echo "autossh is running"
 fi
